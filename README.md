59c49c9f6727fc525a8199a479499bc4add60090
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2019-09-04 14:03:03 +0100

    Added front end build

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Full Stack Open 2019 - Phonebook Backend
 
-Access the application [here]()
+Access the application [here](https://fast-lowlands-18904.herokuapp.com/)
---
+++ build/precache-manifest.cfe7099ca7ed5c1012db89ebb435fa73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e893b1814a4450ebd25f5b3b1a5eb9b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b3b171b50e8e1af50a3",
+    "url": "/static/css/main.c00785b0.chunk.css"
+  },
+  {
+    "revision": "1ad94862c740b6d8d538",
+    "url": "/static/js/2.b78d268b.chunk.js"
+  },
+  {
+    "revision": "7b3b171b50e8e1af50a3",
+    "url": "/static/js/main.d6b1f46c.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
